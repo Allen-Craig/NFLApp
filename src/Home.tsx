dbbf83324a1c371e20c4141fa1f1fdd858dd6824
allen-craig <allen.craig@csaa.com>
2024-10-03 11:54:33 -0700

    added season input feature

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,28 +1,43 @@
 import { useState } from "react";
 
+type TeamData = {
+  name: string;
+  teamID: number;
+};
+
 const Home = () => {
-  const [count, setCount] = useState(0);
+  const [season, setSeason] = useState("");
+  const [response, setResponse] = useState<TeamData>({} as TeamData);
+
   const key = "?key=1a241c5c61df493a988bd27f9d1048ec";
-  const season = "2024REG";
   const url =
     "https://api.sportsdata.io/v3/nfl/scores/json/Standings/" + season + key;
 
-  const fetchData = async () => {
+  const getTeamData = async () => {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
   };
-  fetchData();
 
   return (
     <>
       <h1>Welcome to NFL App</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <button onClick={() => setCount((count) => (count = 0))}>
-        clear count
-      </button>
+      <label htmlFor="Season-input">Input Team Number</label>
+      <input
+        id="Season-input"
+        value={season}
+        type="text"
+        onChange={(event) => setSeason(event.target.value)}
+      />
+      <button onClick={() => getTeamData()}>Get Team Info</button>
+      <div>
+        <p>Name: {response.name}</p>
+      </div>
+      <p className="read-the-docs">
+        Please enter four digit Year value followed by "REG" for regular season,
+        "PRE" for preseason, and "POST" for post season.
+      </p>
+      <p className="read-the-docs">Example 2024REG</p>
     </>
   );
 };
