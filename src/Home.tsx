0448d12539d3bb2fcf6280912d42280db555be96
allen-craig <allen.craig@csaa.com>
2024-10-07 12:37:02 -0700

    WIP on adding team search functionality

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,12 +17,13 @@ type TeamData = {
 };
 
 const Home = () => {
-  const [season, setSeason] = useState("");
+  const [season, setSeason] = useState("2024REG");
+  const [team, setTeam] = useState("");
   const [response, setResponse] = useState<TeamData>({} as TeamData);
   const [isLoading, setIsLoading] = useState(false);
   const key = "?key=1a241c5c61df493a988bd27f9d1048ec";
   const url =
-    "https://api.sportsdata.io/v3/nfl/scores/json/Standings/" + season + key;
+    "https://api.sportsdata.io/v3/nfl/scores/json/Standings/2024REG" + key;
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -36,9 +37,19 @@ const Home = () => {
         throw new Error("Network response was not ok");
       }
       const json = await response.json();
-      setResponse(json[1]);
-      console.log("test", response);
-      console.log("Good data", json.Name);
+
+      // Find the team data by name
+      console.log("Full JSON response:", json);
+      const teamData = Object.values(json).find(
+        (team: any) => team.Name.toLowerCase() === team.Name.toLowerCase()
+      );
+      console.log("Team Data check1", teamData);
+      if (teamData) {
+        setResponse(teamData as TeamData);
+        console.log("Team Data check2", teamData);
+      } else {
+        console.error("Team data not found");
+      }
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
@@ -57,9 +68,16 @@ const Home = () => {
         onChange={(event) => setSeason(event.target.value)}
         placeholder="(YYYYREG/PRE/POST)"
       />
+      <input
+        id="Team-input"
+        value={team}
+        type="text"
+        onChange={(event) => setTeam(event.target.value)}
+        placeholder="(Miami Dolphins)"
+      />
       <button onClick={() => getTeamData()}>Get Team Info</button>
 
-      {console.log("Bad data", response.Name)}
+      {console.log("Team Data check3", response.Name)}
 
       {response && Object.keys(response).length > 0 ? (
         <div className="team-info">
