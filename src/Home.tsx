da0cb7d11826e2665a1c9c9e5ab86e860b610a55
allen-craig <allen.craig@csaa.com>
2024-10-09 09:43:52 -0700

    Updated TeamData to interface

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-type TeamData = {
+interface TeamData {
   SeasonType: number;
   Season: number;
-  Conference: "";
-  Division: "";
-  Team: "";
-  Name: "";
+  Conference: string;
+  Division: string;
+  Team: string;
+  Name: string;
   Wins: number;
   Losses: number;
   Ties: number;
@@ -14,7 +14,7 @@ type TeamData = {
   Touchdowns: number;
   DivisionWins: number;
   TeamID: number;
-};
+}
 
 const Home = () => {
   const [season, setSeason] = useState("2024REG");
