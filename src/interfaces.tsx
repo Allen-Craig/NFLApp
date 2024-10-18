abc4d38dd177fb39c752f180b8f766af1010df26
allen-craig <allen.craig@csaa.com>
2024-10-17 19:15:26 -0700

    Installed Shadcn/UI and Tailwind. Created new file for interfaces

---
+++ src/interfaces.tsx
@@ -0,0 +1,15 @@
+export interface TeamData {
+  SeasonType: number;
+  Season: number;
+  Conference: string;
+  Division: string;
+  Team: string;
+  Name: string;
+  Wins: number;
+  Losses: number;
+  Ties: number;
+  NetPoints: number;
+  Touchdowns: number;
+  DivisionWins: number;
+  TeamID: number;
+}
